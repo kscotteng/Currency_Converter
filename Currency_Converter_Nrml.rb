263b53e682b07ee80ff498b84354eeccf9e3d36d
KBS <KBS@KBSs-MacBook-Pro.local>
2016-02-05 15:39:19 -0500

    add '!=' method; add third currency object; add testing for both

--- Currency_Converter_Nrml.rb
+++ Currency_Converter_Nrml.rb
@@ -15,30 +15,47 @@ class Storage
   end
 
   def ==(second_amount, second_currency)
-    @currency_type_stg == second_currency && @amount_stg == second_amount
+    @currency_type_stg == second_currency.currency &&
+    @amount_stg == second_amount.amount
+  end
+
+  def !=(third_amount, third_currency)
+    @amount_stg != third_amount.amount ||
+    @currency_type_stg != third_currency.currency
   end
 
 end
 
 amount_type = Storage.new(100.0, :USD)
 second = Storage.new(100.0, :USD)
+third = Storage.new(50.0, :EUR)
 
 
 
 
-# test 1 - Current Values in Amount & Currency Methods
-#   puts ""
-#   puts "The current values for Amount & Currency are:"
-#   puts "Amount = #{amount_type.amount}"
-#   puts "Currency = #{amount_type.currency}"
-#   puts ""
-# # test 2 - Testing '==' Method (Full Factorial)
-#   puts ""
-#   puts "Method testing '==':"
-#   dup_test_true = Storage.new(100.0, :USD)
-#     puts "true = #{amount_type.currency == dup_test_true.currency}"
-#     puts "true = #{amount_type.amount == dup_test_true.amount}"
-#   dup_test_false = Storage.new(50.0, :EUR)
-#     puts "false = #{amount_type.currency == dup_test_false.currency}"
-#     puts "false = #{amount_type.amount == dup_test_false.amount}"
-#   puts ""
+# ///test 1 - Current Values in Amount & Currency Methods
+  puts ""
+  puts "The current values for Amount & Currency are:"
+  puts "Amount = #{amount_type.amount}"
+  puts "Currency = #{amount_type.currency}"
+  puts ""
+# ///test 2 - Testing '==' Method (Full Factorial)
+  puts ""
+  puts "Method testing '==':"
+  test_true = Storage.new(100.0, :USD)
+    puts "true = #{amount_type.currency == test_true.currency}"
+    puts "true = #{amount_type.amount == test_true.amount}"
+  test_false = Storage.new(50.0, :EUR)
+    puts "false = #{amount_type.currency == test_false.currency}"
+    puts "false = #{amount_type.amount == test_false.amount}"
+  puts ""
+# ///test 3 - Testing '!=' Method (Full Factorial)
+  puts ""
+  puts "Method testing '!=':"
+  test_true = Storage.new(100.0, :USD)
+    puts "true = #{amount_type.currency == test_true.currency}"
+    puts "true = #{amount_type.amount == test_true.amount}"
+  test_false = Storage.new(50.0, :EUR)
+    puts "false = #{amount_type.currency == test_false.currency}"
+    puts "false = #{amount_type.amount == test_false.amount}"
+  puts ""
