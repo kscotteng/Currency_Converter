2cd9da6403083c0927a5d1ea860edc8e292092e2
KBS <KBS@KBSs-MBP.triad.rr.com>
2016-02-06 23:59:11 -0500

    start over; replace storage class with currency class; add currency method containing an amount and currency code

--- Currency_Converter_Nrml.rb
+++ Currency_Converter_Nrml.rb
@@ -1,61 +1,19 @@
-class Storage
+class Currency
 
-  def initialize(amount, currency_type)
+  def initialize(amount, currency)
     # puts "Testing for inventory"
-    @amount_stg = amount
-    @currency_type_stg = currency_type
+    @amount = amount
+    @currency = currency
   end
 
   def amount
-    @amount_stg
+    @amount
   end
 
   def currency
-    @currency_type_stg
-  end
-
-  def ==(second_amount, second_currency)
-    @currency_type_stg == second_currency.currency &&
-    @amount_stg == second_amount.amount
-  end
-
-  def !=(third_amount, third_currency)
-    @amount_stg != third_amount.amount ||
-    @currency_type_stg != third_currency.currency
+    @currency
   end
 
 end
 
-amount_type = Storage.new(100.0, :USD)
-second = Storage.new(100.0, :USD)
-third = Storage.new(50.0, :EUR)
-
-
-
-
-# ///test 1 - Current Values in Amount & Currency Methods
-  puts ""
-  puts "The current values for Amount & Currency are:"
-  puts "Amount = #{amount_type.amount}"
-  puts "Currency = #{amount_type.currency}"
-  puts ""
-# ///test 2 - Testing '==' Method (Full Factorial)
-  puts ""
-  puts "Method testing '==':"
-  test_true = Storage.new(100.0, :USD)
-    puts "true = #{amount_type.currency == test_true.currency}"
-    puts "true = #{amount_type.amount == test_true.amount}"
-  test_false = Storage.new(50.0, :EUR)
-    puts "false = #{amount_type.currency == test_false.currency}"
-    puts "false = #{amount_type.amount == test_false.amount}"
-  puts ""
-# ///test 3 - Testing '!=' Method (Full Factorial)
-  puts ""
-  puts "Method testing '!=':"
-  test_true = Storage.new(100.0, :USD)
-    puts "true = #{amount_type.currency == test_true.currency}"
-    puts "true = #{amount_type.amount == test_true.amount}"
-  test_false = Storage.new(50.0, :EUR)
-    puts "false = #{amount_type.currency == test_false.currency}"
-    puts "false = #{amount_type.amount == test_false.amount}"
-  puts ""
+first = Currency.new(100.0, :USD)
