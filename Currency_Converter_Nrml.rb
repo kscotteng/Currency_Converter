2c0877be90644aedd162c1fbfdd41061d71c86a3
kscotteng <kscott@gmail.com>
2016-02-07 00:39:08 -0500

    add second object equal to first object in both amount and currency code; add equal method; add testing script, labeled test 1

--- Currency_Converter_Nrml.rb
+++ Currency_Converter_Nrml.rb
@@ -14,6 +14,17 @@ class Currency
     @currency
   end
 
+  def equal(other)
+    @amount == other.amount && @currency == other.currency
+  end
+
 end
 
 first = Currency.new(100.0, :USD)
+second = Currency.new(100.0, :USD)
+
+# ///test 1 - Testing 'equal' Method
+  puts ""
+    puts "Testing 'equal' Method:"
+    puts "true = #{first.equal(second)}"
+  puts ""
