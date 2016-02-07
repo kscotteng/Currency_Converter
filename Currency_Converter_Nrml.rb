b8232b43b4f859c825e44e5524b64fdc8bbada9e
kscotteng <kscott@gmail.com>
2016-02-07 00:45:29 -0500

    add third and fourth objects; add not_equal method; add test scripting for both amount and currency code

--- Currency_Converter_Nrml.rb
+++ Currency_Converter_Nrml.rb
@@ -18,13 +18,27 @@ class Currency
     @amount == other.amount && @currency == other.currency
   end
 
+  def not_equal(other)
+    @amount != other.amount || @currency != other.currency
+  end
+
 end
 
 first = Currency.new(100.0, :USD)
 second = Currency.new(100.0, :USD)
+third = Currency.new(100.0, :EUR)
+fourth = Currency.new(50.0, :USD)
 
 # ///test 1 - Testing 'equal' Method
   puts ""
     puts "Testing 'equal' Method:"
     puts "true = #{first.equal(second)}"
+  puts ""
+
+# ///test 2 - Testing 'not_equal' Method
+  puts ""
+    puts "Testing 'not_equal' Method:"
+    puts "true = #{second.not_equal(third)}"
+    puts "true = #{second.not_equal(fourth)}"
+    puts "true = #{third.not_equal(fourth)}"
   puts ""
